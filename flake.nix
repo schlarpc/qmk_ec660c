daa6577d19e3d96ae06ecbc883ec8ba75ed095e9
Chaz Schlarp <schlarpc@gmail.com>
2025-04-20 22:59:30 +0000

    Use basically stock keymap

--- flake.nix
+++ flake.nix
@@ -8,6 +8,12 @@
       pkgs = nixpkgs.legacyPackages.x86_64-linux;
     in
     {
-      devShells.x86_64-linux.default = pkgs.mkShell { nativeBuildInputs = [ pkgs.qmk ]; };
+      devShells.x86_64-linux.default = pkgs.mkShell {
+        nativeBuildInputs = with pkgs; [
+          qmk
+          nixfmt-rfc-style
+          clang-tools
+        ];
+      };
     };
 }
